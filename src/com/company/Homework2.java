a2b402b0112c0b21d9fd9291cb72366504ece482
user <vardankhublaryan2@gmail.com>
2022-02-10 00:04:43 +0400

    refactoring

---
+++ src/com/company/Homework2.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.io.IOException;
+
+public class Homework2 {
+
+    public static void main(String[] args) throws IOException {
+
+        //havasarakoxm erankyun
+
+        int a = 18;
+        int b = 18;
+        int c = 22;
+
+        if ((a & b) == c) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+        //uxankyun erankyun
+
+        if ((a * a) + (b * b) == (c * c)) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+        //havasarasrun erankyun
+        if (a == b || b == c ){
+            System.out.println(true);
+        }else if (c == a){
+            System.out.println(true);
+        }else {
+            System.out.println(false);
+        }
+
+
+    }
+}
--- src/com/company/homeWork.java
+++ src/com/company/homeWork.java
@@ -3,26 +3,29 @@ package com.company;
 public class homeWork {
 
     public static void main(String[] args) {
-    // հավասարակողմ եռանկյան պարագիծ
-    int a = 18;
-    int b = 18;
-    int c = 18;
+        // հավասարակողմ եռանկյան պարագիծ
+        int a = 18;
+        int b = 18;
+        int c = 18;
 
-    int p = a + b + c;
+        int p = a + b + c;
 
-    System.out.println(p);
+        System.out.println(p);
 
         //  Ուզանկյուն եռանկյան Մակերես
-     int d = 5;
-     int e = 4;
-     int f = 3;
+        int d = 5;
+        int e = 4;
+        int f = 3;
 
-     int s = (e*f)/2;
+        int s = (e * f) / 2;
 
-     System.out.println(s);
+        System.out.println(s);
 //բարձրություն
-     int h = (e*f)/d;
-     System.out.println(h);
+        int h = (e * f) / d;
+        System.out.println(h);
+
+
 
     }
-    }
+}
+
