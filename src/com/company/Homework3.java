b265b4107906cb3d26ae1f5886ae6360cb8ca3ba
Vardan999 <vardankhublaryan2@gmail.com>
2022-02-18 12:53:10 +0400

    updating

---
+++ src/com/company/Homework3.java
@@ -0,0 +1,33 @@
+package com.company;
+import java.io.IOException;
+public class Homework3 {
+    public static void main(String[] args) throws IOException {
+
+        for (int i = 5; i <= 100; i++) {
+            int count = 0;
+            for (int j = 2; j <= i / 2; ++j) {
+                if (i % j == 0) {
+                    count++;
+                }
+
+            }
+            if (count < 1) {
+                System.out.println(i);
+            }
+        }
+
+        for (int k = 0; k <= 100; k++){
+            if (k % 2 == 0){
+                System.out.println(k);
+            }
+        }
+
+        for (int k = 0; k <=100; k++){
+            if (k % 2 == 1){
+                System.out.println(k);
+            }
+        }
+    }
+}
+
+
